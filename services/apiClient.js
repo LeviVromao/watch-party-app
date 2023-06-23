ece32754b04301a0117f5294730a59611aedeb9b
NoOneLevi <zxvitor1@hotmail.com>
2023-06-23 17:42:38 -0300

    aaaaaaaaaa

--- services/apiClient.js
+++ services/apiClient.js
@@ -5,7 +5,7 @@ export function getApiClient(ctx) {
     const { ['auth.token']: token } = parseCookies(ctx);
     
     const api = axios.create({
-        baseURL: 'http://localhost:3000/'
+        baseURL: 'https://watch-party-beta.vercel.app/'
     })
 
     //https://watch-party-beta.vercel.app/
