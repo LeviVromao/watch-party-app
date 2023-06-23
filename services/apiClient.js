0ddea9d9d5217eb82b04cbc0ce32da0d5f1513d9
NoOneLevi <zxvitor1@hotmail.com>
2023-06-23 17:19:29 -0300

    aaaaaa

--- services/apiClient.js
+++ services/apiClient.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { parseCookies } from 'nookies';
 
 export function getApiClient(ctx) {
-    const {['auth.token']: token} = parseCookies(ctx);
+    const { ['auth.token']: token } = parseCookies(ctx);
     
     const api = axios.create({
         baseURL: 'http://localhost:3000/'
@@ -11,13 +11,13 @@ export function getApiClient(ctx) {
     //https://watch-party-beta.vercel.app/
 
     api.interceptors.request.use(config => {
-        
-        if(token) {
-            config.headers.Authorization = token;
-        }
 
         return config
     });
+
+    if(token) {
+        api.defaults.headers.authorization = token;
+    }
 
     return api;
 }
