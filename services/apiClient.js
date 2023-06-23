5b5828a6cc0d5411452c78d7186d6ece5271fb74
NoOneLevi <zxvitor1@hotmail.com>
2023-06-23 16:32:37 -0300

    this thing have to work

--- services/apiClient.js
+++ services/apiClient.js
@@ -5,7 +5,7 @@ export function getApiClient(ctx) {
     const {['auth.token']: token} = parseCookies(ctx);
     
     const api = axios.create({
-        baseURL: 'https://watch-party-beta.vercel.app/'
+        baseURL: 'http://localhost:3000/'
     })
 
     //https://watch-party-beta.vercel.app/
