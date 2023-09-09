95eec1c23325968754922d51009d653d41ff4f51
NoOneLevi <zxvitor1@hotmail.com>
2023-09-09 13:29:35 -0300

    fix: register page css fixed

--- pages/register/index.tsx
+++ pages/register/index.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { useContext } from "react";
 import Link from "next/link";
 import { authContext } from "../..//context/authProvider";
-import styles from '../../styles/Register.module.css';
+import styles from '../../styles/Form.module.css';
 import { AiOutlineArrowRight } from 'react-icons/ai'
 import React from "react";
 
@@ -27,41 +27,40 @@ export default function Register() {
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="https://th.bing.com/th/id/OIG.DKYsTD6pJtVIu0.XWPy6?pid=ImgGn" />
         </Head>
-        <div className={styles.Login_Container}>
-          <main className={styles.main}>
-            <form className={styles.form} onSubmit={handleRegister}>
-              <div className={styles.inputCamp}>
-                <input 
-                  type="email" 
-                  id="email" 
-                  placeholder="Email"
-                  className={styles.inputField} 
-                  onChange={ event => setEmail(event.target.value) }
-                />
-              </div>
-              {error && <i className={styles.error}>{error}</i>}
-              <div className={styles.inputCamp}>
-                <input 
-                  type="password" 
-                  id="password" 
-                  className={styles.inputField} 
-                  placeholder="Senha"
-                  onChange={ event => setPass(event.target.value)}
-                />
-              </div>
-              
-              <div className={styles.inputCamp}>
-                <button 
-                  type="submit"
-                  className={styles.inputSubmit}
-                >
+        <div className={styles.login_container}>
+        <main className={styles.main}>
+          <img src='/chatBallon.svg' alt="Ballon image" />
+          <div className={styles.form_container}>
+              <form className={styles.form} onSubmit={handleRegister}>
+                  <input 
+                    type="email" 
+                    id="email" 
+                    placeholder="Email"
+                    className={styles.input_field} 
+                    onChange={ event => setEmail(event.target.value) }
+                  />
+                  {error && <i className={styles.error}>{error}</i>}
+                  <input 
+                    type="password" 
+                    id="password" 
+                    className={styles.input_field} 
+                    placeholder="Senha"
+                    onChange={ event => setPass(event.target.value)}
+                  />
+                
+                <div className={styles.input_camp}>
+                  <button 
+                    type="submit"
+                    className={`${styles.input_field} ${styles.submit}`}
+                  >
                   < AiOutlineArrowRight className={styles.arrow}/>
-                </button>
-                <Link href="/">Já possui uma conta? faça o login!</Link>
-              </div>
-            </form>
-          </main>
-        </div>
+                  </button>
+                </div>
+                  <Link href="/" className={styles.a}>Já possui uma conta? faça o login!</Link>
+              </form>
+          </div>
+        </main>
+      </div>
       </>
     )
 }
