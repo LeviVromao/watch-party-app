92a5c2f7d1b1b30485dfee51fe4913a1cce715a5
NoOneLevi <zxvitor1@hotmail.com>
2024-03-05 23:00:47 -0300

    🛠️ fix: change the name of the enviroment variables

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,15 +3,15 @@ import GoogleProvider from "next-auth/providers/google";
 import { config } from "dotenv";
 config()
 
-if(!process.env.GOOGLE_SECRET || !process.env.CLIENT_ID){
+if(!process.env.GOOGLE_CLIENT_SECRET || !process.env.GOOGLE_CLIENT_ID){
   throw new Error("An credentials is needed to continue")
 }
 
 export const authOptions = {
   providers : [
     GoogleProvider({
-        clientId: process.env.CLIENT_ID,
-        clientSecret: process.env.GOOGLE_SECRET,
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     })
   ]
 }
